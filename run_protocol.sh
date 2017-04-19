4cde56a6e0f44fa47c76cfe5a1c9448f453157f6
liorbiu <liork.cryptobiu@gmail.com>
2017-04-19 11:33:47 +0300

    change run protocol script to the correct number of arguments

--- run_protocol.sh
+++ run_protocol.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 for i in `seq $1 1 $2`;
 do	
-	./MPCHonestMajority $i $3 $4 output.txt $5 ZpMensenne &
+	./MPCHonestMajority $i $3 $4 output.txt $5 ZpMensenne  $6 $7 $8 &
 	echo "Running $i..."
 done
